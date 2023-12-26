977404a4db9fad4e241512fe6d749c08fe5c8567
BHARGAVA ABHIRAM UMMADISHETTY <91135759+abhiram-ummadishetty@users.noreply.github.com>
2023-12-26 20:25:05 +0530

    version_control

---
+++ version_control.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import datetime
+
+
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
